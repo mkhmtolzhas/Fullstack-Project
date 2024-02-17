3c1e6bf0f53065d70296ec581b48a8fa08582efe
Olzhas Makhmet <mkhmtcore@gmail.com>
2024-02-17 20:07:01 +0600

    added tours

---
+++ src/scripts/fon_about.js
@@ -0,0 +1,13 @@
+VANTA.CLOUDS({
+    el: "#home-content",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    cloudColor: 0x506679,
+    skyColor: 0x01003f,
+    sunColor: 0x0,
+    sunGlareColor: 0x0,
+    sunlightColor: 0x0
+  })
